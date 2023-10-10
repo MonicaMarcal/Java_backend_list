300e93ea82d80cccd18a3ef71e6730498c3651a2
monica.marcal <monicagmarcal@outlook.com>
2023-10-10 07:47:18 -0300

    feat: projeto gerado com spring initializr online

---
+++ todo-do-list/src/main/java/br/com/monica/project/tododolist/TodoDoListApplication.java
@@ -0,0 +1,13 @@
+package br.com.monica.project.tododolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoDoListApplication.class, args);
+	}
+
+}
