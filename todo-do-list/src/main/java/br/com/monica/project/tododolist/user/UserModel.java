5c2810d3532a58d6e82920238d383b439a50d5fa
monica.marcal <monicagmarcal@outlook.com>
2023-10-12 17:31:22 -0300

    feat: manipulan do os valores dos atributos privados

--- todo-do-list/src/main/java/br/com/monica/project/tododolist/user/UserModel.java
+++ todo-do-list/src/main/java/br/com/monica/project/tododolist/user/UserModel.java
@@ -1,7 +1,36 @@
 package br.com.monica.project.tododolist.user;
 
 public class UserModel {
-    public String username;
-    public String name;
-    public String password;
+    private String username;
+    private String name;
+    private String password;
+
+
+ //inserir valor (username)
+  public void setUsername(String username) {
+      this.username = username;
+  }
+
+  //buscar valor (username)
+  public String getUsername() {
+      return username;
+  }
+
+  public void setName(String name) {
+      this.name = name;
+  }
+
+  public String getName() {
+      return name;
+  }
+
+  public void setPassword(String password) {
+      this.password = password;
+  }
+
+  public String getPassword() {
+      return password;
+  }
+
 }
+
