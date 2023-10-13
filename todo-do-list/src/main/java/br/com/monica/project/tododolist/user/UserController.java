7de2e74fb97e5f5bd3c6569b2ee0f3aa4a63d5ea
monica.marcal <monicagmarcal@outlook.com>
2023-10-13 19:56:31 -0300

    feat: hash da senha

--- todo-do-list/src/main/java/br/com/monica/project/tododolist/user/UserController.java
+++ todo-do-list/src/main/java/br/com/monica/project/tododolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -19,11 +21,16 @@ public class UserController {
     //metodo que recebe dados do usuario
     @PostMapping("/")
     public ResponseEntity create(@RequestBody UserModel userModel){
+
             var user = this.userRepository.findByUsername(userModel.getUsername());
             if(user != null){
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario já existe");
             }
-            
+
+            //criptografando senha do usuario
+            var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+            userModel.setPassword(passwordHashed);
+
             var userCreated =  this.userRepository.save(userModel);
             return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
