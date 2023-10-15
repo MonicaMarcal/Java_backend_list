02f209b1981c09db7d8744658a51f3f7b190286b
monica.marcal <monicagmarcal@outlook.com>
2023-10-14 22:03:51 -0300

    feat: criando uma task

---
+++ todo-do-list/src/main/java/br/com/monica/project/tododolist/task/ITaskRepository.java
@@ -0,0 +1,9 @@
+package br.com.monica.project.tododolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository  extends JpaRepository<TaskModel, UUID>{
+    
+}
